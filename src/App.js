2c9f17c51dcc9e78b9f6c4aa2179e31d8276be19
MayankTiwari9 <mt1027319@gmail.com>
2023-11-28 18:10:52 +0530

    Error Message Changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,36 +7,69 @@ import Loader from "./components/Loader";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retryInterval, setRetryInterval] = useState(null);
 
   const fetchMovieHandler = async () => {
     setIsLoading(true);
+    setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong ...Retrying!");
+      }
+
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }));
       setMovies(transformedMovies);
-    } catch (err) {
-      console.log(err);
-    } finally {
       setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      retryFetching();
     }
   };
+
+  const retryFetching = () => {
+    const intervalId = setInterval(() => {
+      fetchMovieHandler();
+    }, 5000);
+    setRetryInterval(intervalId);
+  };
+
+  const cancelRetryHandler = () => {
+    setIsLoading(false);
+    setError("Fetching movies canceled by user.");
+    if (retryInterval) {
+      clearInterval(retryInterval);
+      setRetryInterval(null);
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading && error && !retryInterval) {
+      // Retry only when not already loading and there's an error
+      retryFetching();
+    }
+  }, [isLoading, error, retryInterval, retryFetching]);
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        <button onClick={cancelRetryHandler} disabled={!retryInterval}>
+          Cancel Retry
+        </button>
       </section>
       <section>
-        {isLoading ? <Loader/> : <MoviesList movies={movies} />}
+        {isLoading ? <Loader /> : <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
